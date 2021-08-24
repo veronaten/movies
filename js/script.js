9d4519325a13e7ac74038451b0118ce786d3da57
Veronika <verona.binko@gmail.com>
2021-08-24 23:13:51 +0300

    make clean code

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-/* Задания на урок:
-
-1) Удалить все рекламные блоки со страницы (правая часть сайта)
-
-2) Изменить жанр фильма, поменять "комедия" на "драма"
-
-3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
-Реализовать только при помощи JS
-
-4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
-
-5) Добавить нумерацию выведенных фильмов */
-
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -40,9 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let newFilm = addInput.value;
 
     const favorite = checkbox.checked;
-
     
-
     if (newFilm) {
       
       if (newFilm.length > 21) {
@@ -100,17 +84,8 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     });
   }
-
-   
+ 
   makeChanges(); 
   deleteAdv(promoAdv);
   createMovieList(movieDB.movies, movieList);
 })
-
-
-
-
-
-
-
-
